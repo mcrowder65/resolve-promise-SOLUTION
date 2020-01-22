6ebacd26f64e47120183e4b967cc7f8ca381f4bd
Matt Crowder <mcrowder65@gmail.com>
2020-01-22 18:26:17 -0500

    Initial commit

---
+++ utilities.js
@@ -0,0 +1,36 @@
+function sleep(time) {
+  return new Promise(resolve => {
+    setTimeout(resolve, time)
+  })
+}
+
+function getDogBreeds() {
+  return sleep(0).then(() => {
+    return [
+      {
+        id: "2f349b0c",
+        name: "Golden Retriever"
+      },
+      {
+        id: "ea382216",
+        name: "Corgi"
+      },
+      {
+        id: "6f0e0d6e",
+        name: "Yellow Lab"
+      },
+      {
+        id: "e6450423",
+        name: "Pit Bull"
+      },
+      {
+        id: "2641706a",
+        name: "German Shepherd"
+      }
+    ]
+  })
+}
+
+module.exports = {
+  getDogBreeds
+}
